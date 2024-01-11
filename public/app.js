16e57f6f26608e388d94fab416e4be9188bc65a8
Quosain Hasan <33932120+quosainhasan@users.noreply.github.com>
2024-01-11 18:36:04 +0000

    Add console.log statement to window.onload function

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 window.onload = async () => {
+    console.log('window.onload');
     const response = await fetch('/data');
     const data = await response.json();
     const dataDiv = document.getElementById('data');
