9ebe39c900605b4178f5cdd381703ca929827a16
Quosain Hasan <33932120+quosainhasan@users.noreply.github.com>
2024-01-12 09:49:16 +0000

    Add CORS support to server.js

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const app = express();
 const port = 4000;
 const dirname = 'public';
 const fs = require('fs');
+const cors = require('cors');
 
 app.use(express.static('public'));
+app.use(cors());
 
 // app.get('/', (req, res) => {
 //     res.sendFile('./index.html', { root: dirname });
 // });
+
+cors({ credentials: true, origin: true ,
+    fetchOptions: {
+        mode: 'no-cors',
+    },
+
+});
 
 app.get('/data', (req, res) => {
     const data = JSON.parse(fs.readFileSync('data.json'));
